186401d795dabfaaade94f89278a6b84b7a7f5fb
Rodrigo de Almeida <rodrigoalmeida.as@gmail.com>
2019-01-05 10:44:09 -0200

    Initial commit

---
+++ BillsConstrolAPI/src/main/java/br/com/billscontrol/api/BillsConstrolApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.billscontrol.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsConstrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsConstrolApiApplication.class, args);
+	}
+
+}
+
---
+++ BillsConstrolAPI/src/test/java/br/com/billscontrol/api/BillsConstrolApiApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.billscontrol.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillsConstrolApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
